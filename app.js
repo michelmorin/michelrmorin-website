2a7c06f8c01d3bacc59f01dc84b070a6b2c3a111
michelmorin <michelmorin77@gmail.com>
2020-05-17 12:51:31 -0400

    #1 First part of the landing page

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
 // Required dependencies
-const express = require('express');
-const bodyParser = require('body-parser');
-require('dotenv').config();
+const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 //App Config
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 //app.use(methodOverride('_method'));
 
 // Routes
-app.get('/', (req, res) => {
-	res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-const port = process.env.PORT || 3000;
-
-app.listen(port, function() {
-	console.log(`Server Started on port: ${port}`);
+app.listen(5000, function () {
+  console.log("Server Started!");
 });
