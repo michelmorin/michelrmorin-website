6243b23912cf4fd536933dd9376b59b4e82434f4
michelmorin <michelmorin77@gmail.com>
2021-02-10 15:18:21 -0500

    fixes to port from .env file and setup doc

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 // Required dependencies
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
+const express = require("express")
+const app = express()
+const bodyParser = require("body-parser")
+const dotenv = require('dotenv').config()
 
 //App Config
-app.set("view engine", "ejs");
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(methodOverride('_method'));
+app.set("view engine", "ejs")
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // Routes
-app.get("/", (req, res) => {
-  res.render("index");
-});
+app.get("/", (req, res) => res.render("index"));
 
 const PORT = process.env.PORT || 5000;
 
